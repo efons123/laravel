cfe0894274cc349d7453b3e2ce2169d0c2037983
efons123 <efanony7@gmail.com>
2024-11-26 17:30:37 +0700

    Laravel

---
+++ web/app/Filament/Widgets/WidgetIncomeChart.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use App\Models\Transaction;
+use Carbon\Carbon;    
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+
+class WidgetIncomeChart extends ChartWidget
+{
+    use InteractsWithPageFilters;
+    protected static ?string $heading = 'Pemasukan';
+    protected static string $color = 'success';
+
+    protected function getData(): array
+    {
+
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+        Carbon::parse($this->filters['startDate']) :
+        null;
+
+    $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+        Carbon::parse($this->filters['endDate']) :
+        now();
+        $data = Trend::query(Transaction::incomes())
+        ->between(
+            start: $startDate,
+            end: $endDate,
+        )
+        ->perDay()
+        ->count();
+ 
+    return [
+        'datasets' => [
+            [
+                'label' => 'Pemasukan Per Hari',
+                'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+            ],
+        ],
+        'labels' => $data->map(fn (TrendValue $value) => $value->date),
+    ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
